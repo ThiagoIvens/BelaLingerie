c659eddf3df2d659cff4b5156bd855daa394b7ee
ThiagoIvens <thiagorech2121@gmail.com>
2021-06-10 05:39:14 -0300

    carousel

--- ecommerce/models.py
+++ ecommerce/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Products(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=255, blank=False)
+    description = models.TextField(blank=False)
+    image = models.FileField(upload_to="media/%Y/%m/%d/")

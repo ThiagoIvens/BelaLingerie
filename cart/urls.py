0e7f6a2cd1a75fa4fa8c611b22bd911d34c1851d
ThiagoIvens <thiagorech2121@gmail.com>
2021-07-08 17:10:57 -0300

    resolvido erro de session adição e atualização de finalizar compra, produtos, e estilização + ajuste de responsividade

--- cart/urls.py
+++ cart/urls.py
@@ -1,11 +1,11 @@
-from django.urls import path, include
-
+from django.urls import path
+from django.urls.conf import re_path
 from .views import cart_detail, cart_add, cart_remove
 
 app_name = "cart"
 
 urlpatterns = [
     path("", cart_detail, name="detail"),
-    path("add/<int:product_id>/", cart_add, name="add"),
-    path("remove/<int:product_id>/", cart_remove, name="remove"),
+    path("add/<int:product_id>", cart_add, name="add"),
+    path("remove/<int:product_id>", cart_remove, name="remove"),
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,12 +1,11 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from django.utils.translation import override
+from django.shortcuts import get_object_or_404, redirect, render
 from django.views.decorators.http import require_POST
 from products.models import Product
 
 from .cart import Cart
 from .forms import CartAddProductForm
 
-# Create your views here.
+
 @require_POST
 def cart_add(request, product_id):
     cart = Cart(request)
@@ -16,18 +15,20 @@ def cart_add(request, product_id):
     if form.is_valid():
         cd = form.cleaned_data
         cart.add(
-            product=product, quantity=cd['quantity'], override_quantity=cd['override'],
+            product=product, quantity=cd["quantity"], override_quantity=cd["override"]
         )
 
-    return redirect('cart:detail')
+    return redirect("cart:detail")
+
 
 @require_POST
 def cart_remove(request, product_id):
     cart = Cart(request)
     product = get_object_or_404(Product, id=product_id)
     cart.remove(product)
-    return redirect('cart:detail')
+    return redirect("cart:detail")
+
 
 def cart_detail(request):
     cart = Cart(request)
-    return render(request, 'cart/sacola.html', {'cart': cart})
+    return render(request, "cart/sacola.html", {"cart": cart})
